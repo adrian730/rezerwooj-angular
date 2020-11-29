b2f9fd34416e1819c3f139fb92b07fe114a25b3e
adrian730 <antosz.a@poczta.fm>
2020-11-29 12:47:11 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CustomerViewComponent } from './customer-view/customer-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
